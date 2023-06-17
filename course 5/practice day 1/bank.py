327564424b8f671d9b9a01d2dcf853768dc1af20
MD Jafor Sadek Khan <116980696+MD-Jafor-Sadek-Khan@users.noreply.github.com>
2023-06-17 11:36:46 +0600

    final exam project done

--- course 5/practice day 1/bank.py
+++ course 5/practice day 1/bank.py
@@ -17,7 +17,7 @@ Admin
 -Can create an account  --> DONE
 -Can check the total available balance of the bank. --> DONE
 -Can check the total loan amount.  --> DONE
--Can on or off the loan feature of the bank.
+-Can on or off the loan feature of the bank. ---> DONE
 
 
 special notes:
@@ -27,17 +27,36 @@ Note
 -If a user is unable to withdraw the amount of money he has deposited in the bank, he will get a message that the bank is bankrupt. ---> DONE
 
 """
+
+
+#import section
+
 import datetime
 
+
+#common classes
 
 class Bank:
     totalBalance = 0
     totalLoanAmount = 0
-    loanFeatureToggle=True
+
     def __init__(self, name) -> None:
         self.name = name
+        self.loanFeatureToggle = True
 
-    
+    # @property
+    # def loanAllowenceToggle(self):
+    #     return self.bank.loanFeatureToggle
+
+    # @loanAllowenceToggle.setter
+    # def loanAllowenceToggle(self, value):
+    #     self.bank.loanFeatureToggle = value
+
+    def toggleLoanFeature(self, value):
+        self.loanFeatureToggle = value
+
+
+#Child classes
 
 class User(Bank):
     def __init__(self, name, email, password, bank) -> None:
@@ -55,7 +74,7 @@ class User(Bank):
     def transferAmount(self, amount, accountID):
         if amount > 0 and amount <= self.balance:
             transactionTime = datetime.datetime.now()
-            transactionStatus = f"Transfered an amount of {amount}tk to user \"{accountID.name}\" on {transactionTime}"
+            transactionStatus = f'Transfered an amount of {amount}tk to user "{accountID.name}" on {transactionTime}'
             self.transactionHistory = transactionStatus
             accountID.balance += amount
             self.balance -= amount
@@ -65,30 +84,35 @@ class User(Bank):
             else:
                 print(f"Insufficient Funds")
 
-    
-    
     def getLoan(self, amount):
-        if amount <= self.bank.totalBalance:
-            if amount > self.balance * 2:
-                print(
-                    f"You cant take a loan beyound 2 times your current balance.\nYour Current Balance is {self.balance}tk\nYour eligibility for load is upto amount = {self.balance*2}tk"
-                )
+        if self.bank.loanFeatureToggle:
+            if amount <= self.bank.totalBalance:
+                if amount > self.balance * 2:
+                    print(
+                        f"You cant take a loan beyound 2 times your current balance.\nYour Current Balance is {self.balance}tk\nYour eligibility for load is upto amount = {self.balance*2}tk"
+                    )
+                else:
+                    transactionTime = datetime.datetime.now()
+                    transactionStatus = (
+                        f"Took a Loan of {amount}tk on {transactionTime}"
+                    )
+                    self.transactionHistory = transactionStatus
+                    self.bank.totalBalance -= amount
+                    self.bank.totalLoanAmount += amount
+                    print(
+                        f"Heres your loan of {amount}tk. Happy Banking!! Better pay up in time..or else!!"
+                    )
             else:
-                transactionTime = datetime.datetime.now()
-                transactionStatus = f"Took a Loan of {amount}tk on {transactionTime}"
-                self.transactionHistory = transactionStatus
-                self.bank.totalBalance -= amount
-                self.bank.totalLoanAmount += amount
-                print(
-                    f"Heres your loan of {amount}tk. Happy Banking!! Better pay up in time..or else!!"
-                )
+                print(f"Sorry! currently You can get a loan of upto {self.bank.totalBalance} due to the bank having insufficient funds")
         else:
-            print(f"You can get a loan of upto {self.bank.totalBalance}")
+            print(f"Currently {self.bank.name} Bank isnt issuing any loans.Thank you")
 
     @property
     def transactionHistory(self):
+        print(f'\n\n Your Transaction History in {self.bank.name} Bank \n\n')
         for i in self.transactionHistoryList:
             print(i)
+        print(f"\n\n**************************************************\n\n")
         return ""
 
     @transactionHistory.setter
@@ -102,7 +126,7 @@ class User(Bank):
             transactionStatus = f"Deposited {amount}tk on {transactionTime}"
             self.transactionHistory = transactionStatus
             self.bank.totalBalance += amount
-            return f"successfully deposited {amount} in your account"
+            print(f"successfully deposited {amount} in your account")
         else:
             print(f"The amount cant be negative")
 
@@ -115,7 +139,7 @@ class User(Bank):
                 self.transactionHistory = transactionStatus
                 self.bank.totalBalance -= amount
 
-                return f"widraw successful you got {amount}"
+                print(f"widraw successful you got {amount}")
             else:
                 if amount < 0:
                     print(f"Widraw amount cant be negative")
@@ -133,6 +157,13 @@ class Admin(Bank):
         self.bank = bank
         super().__init__(name)
 
+    
+    def LoanFeatureToggle(self,value):
+        self.bank.toggleLoanFeature(value)
+    # def Turn_On_Or_Off_Loan_Feature(self,value):
+    #     self.bank.loanAllowenceToggle(value)
+        
+    
     @property
     def getBanksTotalBalance(self):
         total = self.bank.totalBalance
@@ -143,23 +174,28 @@ class Admin(Bank):
 
     @property
     def getTotalLoanAmount(self):
-        total= self.bank.totalLoanAmount
-        print(f'{self.bank.name} Banks total Loan Amount is {total}')
+        total = self.bank.totalLoanAmount
+        print(f"{self.bank.name} Banks total Loan Amount is {total}")
+
+
+
+
+#testing Phase
 
 dbbl = Bank("dbbl")
 dbbluser1 = User("kala", "kala@bazar.com", 12345, dbbl)
 print(dbbluser1.balance)
-print(dbbluser1.transactionHistory)
+dbbluser1.transactionHistory
 dbbluser1.deposite(500)
 dbbluser1.getBalance
 dbbluser1.deposite(500)
 dbbluser1.getBalance
-print(dbbluser1.transactionHistory)
+dbbluser1.transactionHistory
 dbbluser1.widraw(200)
 dbbluser1.getBalance
 dbbluser1.widraw(900)
 dbbluser1.getBalance
-print(dbbluser1.transactionHistory)
+dbbluser1.transactionHistory
 dbbluser1.getLoan(500)
 
 user2 = User("ami manush", "dhola@gmail.com", 12345, dbbl)
@@ -214,15 +250,21 @@ dbblAdmin1.getTotalLoanAmount
 
 dbbluser1.transactionHistory
 
-dbbluser2=User("hum","u",12,dbbl)
+dbbluser2 = User("hum", "u", 12, dbbl)
 
 dbbluser2.deposite(1200)
 dbbluser2.getLoan(100)
 dbbluser2.getLoan(200)
+dbblAdmin1.LoanFeatureToggle(False)
 dbbluser2.getLoan(400)
 dbbluser2.getLoan(800)
 dbbluser2.getBalance
 dbbluser2.widraw(1200)
+brackUser1.getLoan(2)
 dbbluser2.transactionHistory
 dbblAdmin1.getTotalLoanAmount
 dbblAdmin1.getBanksTotalBalance
+dbbluser1.getBalance
+dbbluser1.getLoan(1)
+brackUser1.getBalance
+brackUser1.getLoan(10)
--- course 5/practice day 1/tempCodeRunnerFile.py
+++ course 5/practice day 1/tempCodeRunnerFile.py
@@ -1,14 +1,1 @@
- print(
-                    f"You cant take a loan beyound 2 times your current balance.\nYour Current Balance is {self.balance}tk\nYour eligibility for load is upto amount = {self.balance*2}tk"
-                )
-            else:
-                transactionTime = datetime.datetime.now()
-                transactionStatus = f"Took a Loan of {amount}tk on {transactionTime}"
-                self.transactionHistory = transactionStatus
-                self.bank.totalBalance -= amount
-                self.bank.totalLoanAmount += amount
-                print(
-                    f"Heres your loan of {amount}tk. Happy Banking!! Better pay up in time..or else!!"
-                )
-        else:
-            print(f"You can get a loan of upto {self.bank.totalBalance}")
+toggleLoanFeatureToggle
